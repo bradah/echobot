d93413e48c5640887993b243e53014c9e50b2a5c
Oleg Pascal <rtrn.0@ya.ru>
2020-09-09 20:34:02 +0300

    Added some imports in Methods.hs

--- src/Telegram/Bot/API/Methods.hs
+++ src/Telegram/Bot/API/Methods.hs
@@ -1,3 +1,8 @@
 module Telegram.Bot.API.Methods where
 
+import           Network.HTTP.Req
+import           Telegram.Bot.API.Request
+import           Telegram.Bot.API.Types
+
 -- * This file contains availabe methods
+
