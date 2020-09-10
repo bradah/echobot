0ecdf9322ea65d71ee52b8bcb69082ee19826a6b
Oleg Pascal <rtrn.0@ya.ru>
2020-09-10 15:46:52 +0300

    Added types for request parameters

--- src/Telegram/Bot/API/Request.hs
+++ src/Telegram/Bot/API/Request.hs
@@ -1,11 +1,15 @@
-{-# LANGUAGE DeriveGeneric   #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module Telegram.Bot.API.Request where
 
 import           Data.Aeson
 import           Data.Text                            (Text)
 import           GHC.Generics
+import           Network.HTTP.Req
 import           Telegram.Bot.API.Internal.Derivation
+import           Telegram.Bot.API.Types
 
 -- * Types representing Telegram response
 
@@ -20,4 +24,28 @@ data Response a = Response
   , responseErrorCode   :: Maybe Int
   } deriving (Show, Generic)
 
+
+botBaseUrl :: Text -> Url Https
+botBaseUrl token = https "api.telegram.org" /: "bot" <> token
+
+-- | Request body for 'sendMessage'
+data SendMessageRequest = SendMessageRequest
+  { sendMessageChatId :: ChatId
+  , sendMessageText   :: Text
+  } deriving (Generic)
+
+-- | Request body for 'getUpdates'
+data GetUpdatesRequest = GetUpdatesRequest
+  { getUpdatesOffset         :: UpdateId
+  , getUpdaetsAllowedUpdates :: Maybe [UpdateType]
+  } deriving (Generic)
+
+-- | Types of updates allowed to receive
+data UpdateType =
+  UpdateMessage
+  deriving (Generic)
+
+deriveJSON' ''SendMessageRequest
+deriveJSON' ''UpdateType
+deriveJSON' ''GetUpdatesRequest
 deriveJSON' ''Response
