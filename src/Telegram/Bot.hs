de6871c66413d70d1091011968b336e2e5f1ecc5
Oleg Pascal <rtrn.0@ya.ru>
2020-09-11 13:29:03 +0300

    Added Telegram.Reply

--- src/Telegram/Bot.hs
+++ src/Telegram/Bot.hs
@@ -2,11 +2,13 @@ module Telegram.Bot (
     module Telegram.Methods,
     module Telegram.Request,
     module Telegram.Types,
-    module Telegram.UpdateParser
+    module Telegram.UpdateParser,
+    module Telegram.Reply
 ) where
 
 
 import           Telegram.Methods
+import           Telegram.Reply
 import           Telegram.Request
 import           Telegram.Types
 import           Telegram.UpdateParser
