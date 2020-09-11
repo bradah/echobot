b5417f5a4845bd9b8532fbbf3a0ff1632d107161
Oleg Pascal <rtrn.0@ya.ru>
2020-09-11 10:14:38 +0300

    Added replyText

--- src/Telegram/Reply.hs
+++ src/Telegram/Reply.hs
@@ -1,1 +1,10 @@
 module Telegram.Reply where
+
+import           Data.Text        (Text)
+import           Telegram.Methods
+import           Telegram.Request
+import           Telegram.Types
+
+replyText :: Token -> ChatId -> Text -> IO (Response (Message))
+replyText token cid t =
+    sendMessage token $ SendMessageRequest cid t
