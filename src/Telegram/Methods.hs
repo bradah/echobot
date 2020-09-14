3a327c9ec0a4cfc4c95406f93cf9c33b75d4153b
Oleg Pascal <rtrn.0@ya.ru>
2020-09-14 11:06:35 +0300

    Swtching to mtl interface

--- src/Telegram/Methods.hs
+++ src/Telegram/Methods.hs
@@ -2,6 +2,7 @@
 module Telegram.Methods where
 
 import           Control.Monad.IO.Class
+import           Control.Monad.Reader
 import qualified Data.ByteString.Char8  as B
 import           Network.HTTP.Req
 import           Telegram.Request
@@ -14,40 +15,60 @@ import           Telegram.Types
 -- | A simple method for testing your bot's auth token.
 --   Requires no parameters. Returns basic information about
 --   the bot in form of a 'User' object.
-getMe :: Token -> IO (Response User)
-getMe token = runReq defaultHttpConfig $ do
-  resp <- req GET
-            (botBaseUrl token /: "getMe")
-            NoReqBody
-            jsonResponse
-            mempty
-  liftIO . return $ (responseBody resp)
+
+type Method body res = ReaderT (Payload body) IO (Response res)
+
+data Payload body = Payload
+  {
+    payloadToken       :: Token
+  , payloadRequestBody :: Maybe body
+  }
+
+runMethod :: Method body res -> Payload body -> IO (Response res)
+runMethod = runReaderT
+
+getMe :: Method a User
+getMe = do
+  token <- asks payloadToken
+  runReq defaultHttpConfig $ do
+    resp <- req GET
+              (botBaseUrl token /: "getMe")
+              NoReqBody
+              jsonResponse
+              mempty
+    liftIO . return $ (responseBody resp)
 
 -- ** sendMessage
 
 -- | Use this method to send text messages.
 --   On success, the sent 'Message' is returned.
-sendMessage :: Token -> SendMessageRequest -> IO (Response Message)
-sendMessage token reqbody = runReq defaultHttpConfig $ do
-  json <- req POST
-            (botBaseUrl token /: "sendMessage")
-            (ReqBodyJson reqbody)
-            jsonResponse
-            mempty
-  liftIO . return $ (responseBody json)
+sendMessage :: Method SendMessageRequest Message
+sendMessage = do
+  token <- asks payloadToken
+  reqbody <- asks payloadRequestBody
+  runReq defaultHttpConfig $ do
+    json <- req POST
+              (botBaseUrl token /: "sendMessage")
+              (ReqBodyJson reqbody)
+              jsonResponse
+              mempty
+    liftIO . return $ (responseBody json)
 
 -- ** getUpdates
 
 -- | Use this method to receive incoming updates.
 --   An Array of 'Update' objects is returned.
-getUpdates :: Token -> GetUpdatesRequest -> IO (Response [Update])
-getUpdates token reqbody = runReq defaultHttpConfig $ do
-  resp <- req POST
-            (botBaseUrl token /: "getUpdates")
-            (ReqBodyJson reqbody)
-            jsonResponse
-            mempty
-  liftIO . return $ (responseBody resp)
+getUpdates :: Method GetUpdatesRequest [Update]
+getUpdates = do 
+  token <- asks payloadToken
+  reqbody <- asks payloadRequestBody
+  runReq defaultHttpConfig $ do
+    resp <- req POST
+              (botBaseUrl token /: "getUpdates")
+              (ReqBodyJson reqbody)
+              jsonResponse
+              mempty
+    liftIO . return $ (responseBody resp)
 
 -- ** sendSticker
 
@@ -55,8 +76,11 @@ getUpdates token reqbody = runReq defaultHttpConfig $ do
 --   animated .TGS stickers. On success, the sent
 --   'Message' is returned.
 
-sendSticker :: Token -> SendStickerRequest -> IO (Response (Message))
-sendSticker token reqbody = runReq defaultHttpConfig $ do
+sendSticker :: Method SendMessageRequest [Message] 
+sendSticker = do
+  token <- asks payloadToken
+  reqbody <- asks payloadRequestBody
+  runReq defaultHttpConfig $ do
   resp <- req POST
             (botBaseUrl token /: "sendSticker")
             (ReqBodyJson reqbody)
