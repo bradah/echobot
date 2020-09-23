e3bf691ad5756520915767f5331936b4872e9b65
Oleg Pascal <rtrn.0@ya.ru>
2020-09-24 00:17:51 +0300

    Add basic VK types

--- src/VK/Methods/Request.hs
+++ src/VK/Methods/Request.hs
@@ -1,1 +1,19 @@
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE TemplateHaskell #-}
 module VK.Methods.Request where
+
+import           Data.Int        (Int32)
+import           Data.Text       (Text)
+import           GHC.Generics
+import           VK.Types
+import           VK.Types.Derive
+
+data Response = Response
+  { reponseTs      :: Maybe Ts
+  , reponseUpdates :: [Update]
+  , responseFailed :: Int32
+  } deriving (Show, Generic)
+
+type Ts = Text
+
+deriveJSON' ''Response
--- src/VK/Types.hs
+++ src/VK/Types.hs
@@ -1,1 +1,194 @@
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE TemplateHaskell #-}
 module VK.Types where
+
+import           Data.Int              (Int32)
+import           Data.Text             (Text)
+import           Data.Time.Clock.POSIX (POSIXTime)
+import           GHC.Generics
+import           VK.Types.Derive
+
+
+data Update = Update
+  { updateType   :: UpdateType
+  , updateObject :: Object
+  } deriving (Show, Generic)
+
+data UpdateType
+  = UpdateTypeMessageNew
+  | UpdateTypeMessageReply
+  | UpdateTypeMessageEdit
+  | UpdateTypeMessageAllow
+  | UpdateTypeMessageDeny
+  | UpdateTypeMessageTypingState
+  | UpdateTypeMessageEvent
+  deriving (Show, Generic)
+
+data Object = Object
+  { objectMessage    :: Message
+  } deriving (Show, Generic)
+
+data Message = Message
+  { messageId           :: MessageId
+  , messageDate         :: POSIXTime
+  , messagePeerId       :: UserId
+  , messageFromId       :: UserId
+  , messageText         :: Text
+  , messageRandomId     :: RandomId
+  , messageRef          :: Text
+  , messageRefSource    :: Text
+  , messageAttachments  :: [Attachment]
+  , messageImportant    :: Bool
+  , messagePayload      :: Text
+  , messageKeyboard     :: Keyboard
+  , messageFwdMessages  :: [Message]
+  , messageReplyMessage :: Message
+  } deriving (Show, Generic)
+
+type RandomId = Int32
+type MessageId = Int32
+
+data Attachment = Attachment
+  { attachmentType        :: AttachmentType
+  , attachmentPhoto       :: Maybe Photo
+  , attachmentVideo       :: Maybe Video
+  , attachmentAudio       :: Maybe Audio
+  , attachmentDoc         :: Maybe Doc
+  , attachmentLink        :: Maybe Link
+  , attachmentMarket      :: Maybe Market
+  , attachmentMarketAlbum :: Maybe MarketAlbum
+  , attachmentWall        :: Maybe Wall
+  , attachmentWallReply   :: Maybe WallReply
+  , attachmentSticker     :: Maybe Sticker
+  } deriving (Show, Generic)
+
+data AttachmentType
+  = AttachmentPhoto
+  | AttachmentVideo
+  | AttachmentAudio
+  | AttachmentDoc
+  | AttachmentLink
+  | AttachmentMarket
+  | AttachmentMarketAlbum
+  | AttachmentWall
+  | AttachmentWallReply
+  | AttachmentSticker
+  | AttachmentGift
+  deriving (Show, Generic)
+
+data Photo = Photo
+  { photoId      :: PhotoId
+  } deriving (Show, Generic)
+
+type PhotoId = Int32
+
+data Video = Video
+  { videoId :: VideoId
+  } deriving (Show, Generic)
+
+type VideoId = Int32
+
+data Audio = Audio
+  { audioId :: AudioId
+  } deriving (Show, Generic)
+
+type AudioId = Int32
+
+data Doc = Doc
+  { docId :: DocId
+  } deriving (Show, Generic)
+
+type DocId = Int32
+
+data Link = Link
+  { linkUrl        :: Text
+  , linkTitle      :: Text
+  , linkCaption    :: Maybe Text
+  , linkDecription :: Text
+  , linkPhoto      :: Maybe Photo
+  } deriving (Show, Generic)
+
+data Market = Market
+  { marketId :: MarketId
+  } deriving (Show, Generic)
+
+type MarketId = Int32
+
+data MarketAlbum = MarketAlbum
+  { marketAlbumId :: MarketAlbumId
+  } deriving (Show, Generic)
+
+type MarketAlbumId = Int32
+
+data Wall = Wall
+  { wallId :: WallId
+  } deriving (Show, Generic)
+
+type WallId = Int32
+
+data WallReply = WallReply
+  { wallReplyId :: WallReplyId
+  } deriving (Show, Generic)
+
+type WallReplyId = Int32
+
+data Sticker = Sticker
+  { stickerStickerId :: StickerId
+  } deriving (Show, Generic)
+
+type StickerId = Int32
+
+data Keyboard = Keyboard
+  { keyboardOneTime :: Bool
+  , keyboardButton  :: [[Button]]
+  , keyboardInline  :: Bool
+  } deriving (Show, Generic)
+
+data Button = Button
+  { buttonAction :: ButtonAction
+  , buttonColor  :: ButtonColor
+  } deriving (Show, Generic)
+
+data ButtonColor
+  = ButtonColorPrimary
+  | ButtonColorSecondary
+  | ButtonColorNegative
+  | ButtonColorPositive
+  deriving (Show, Generic)
+
+data ButtonAction = ButtonAction
+  { buttonActionType    :: ButtonActionType
+  , buttonActionLabel   :: Maybe Text
+  , buttonActionPayload :: Payload
+  } deriving (Show, Generic)
+
+data ButtonActionType
+  = ButtonActionText
+  deriving (Show, Generic)
+
+type Payload = Text
+
+type UserId = Int32
+
+
+deriveJSON' ''ButtonActionType
+deriveJSON' ''ButtonAction
+deriveJSON' ''ButtonColor
+deriveJSON' ''Button
+deriveJSON' ''Keyboard
+deriveJSON' ''Sticker
+deriveJSON' ''WallReply
+deriveJSON' ''Wall
+deriveJSON' ''MarketAlbum
+deriveJSON' ''Market
+deriveJSON' ''Photo
+deriveJSON' ''Link
+deriveJSON' ''Doc
+deriveJSON' ''Audio
+deriveJSON' ''Video
+deriveJSON' ''AttachmentType
+deriveJSON' ''Attachment
+deriveJSON' ''Message
+deriveJSON' ''Object
+deriveJSON' ''UpdateType
+deriveJSON' ''Update
