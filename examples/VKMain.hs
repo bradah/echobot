7744a7e7ce585f085263ad3104c6a654b174b550
Oleg Pascal <rtrn.0@ya.ru>
2020-09-24 17:31:43 +0300

    Add VK.Env

--- examples/VKMain.hs
+++ examples/VKMain.hs
@@ -2,10 +2,18 @@
 module Main where
 
 import qualified Data.Configurator as Conf
-import qualified Data.Text.IO      as TIO
 import           VK.Bot
 import           VK.Env
 
 main :: IO ()
 main = do
-  putStrLn "not yet implemented"
+  conf <- Conf.load [Conf.Required "echobot.conf.local"]
+  mbToken <- Conf.lookup conf "vk.token"
+  mbGroupId <- Conf.lookup conf "vk.group_id"
+  case mbToken of
+    Nothing -> error "echobot.conf.local: no vk.token"
+    Just token -> case mbGroupId of
+      Nothing  -> error "echobot.conf.local: no vk.group_id"
+      Just gId -> do
+        env <- mkEnv token gId
+        print env
--- src/VK/Env.hs
+++ src/VK/Env.hs
@@ -1,1 +1,62 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 module VK.Env where
+
+import           Control.Monad.IO.Class
+import           Data.Int               (Int32)
+import           Data.Text              (Text)
+import           GHC.Generics
+import           Network.HTTP.Req
+import           VK.Methods.Request
+import           VK.Types.Derive
+
+data Env = Env
+  { envToken     :: Text
+  , envGroupId   :: GroupId
+  , envLPSServer :: LPSServer
+  , envLPSKey    :: LPSKey
+  , envTs        :: Ts
+  } deriving (Show)
+
+type GroupId = Text
+type LPSServer = Text
+type LPSKey = Text
+type Token = Text
+
+mkEnv :: Token -> GroupId -> IO Env
+mkEnv token gId = do
+  lpsResult <- runReq defaultHttpConfig $ do
+    let params = "group_id" =: gId
+              <> "access_token" =: token
+              <> "v" =: ("5.124" :: Text)
+    resp <- req POST
+                (https "api.vk.com" /: "method" /: "groups.getLongPollServer")
+                (ReqBodyUrlEnc params)
+                jsonResponse
+                mempty
+    liftIO . return $ (responseBody resp :: GetLPSResult)
+
+  case getLPSResultResponse lpsResult of
+    Nothing -> error "mkEnv: no Response"
+    Just response -> return $ Env
+      { envToken = token
+      , envGroupId = gId
+      , envLPSServer = getLPSResponseServer response
+      , envLPSKey = getLPSResponseKey response
+      , envTs = getLPSResponseTs response
+      }
+
+data GetLPSResult =
+  GetLPSResult
+  { getLPSResultResponse :: Maybe GetLPSResponse
+  } deriving (Generic)
+
+data GetLPSResponse = GetLPSResponse
+  { getLPSResponseKey    :: LPSKey
+  , getLPSResponseServer :: LPSServer
+  , getLPSResponseTs     :: Ts
+  } deriving (Generic)
+
+deriveJSON' ''GetLPSResponse
+deriveJSON' ''GetLPSResult
