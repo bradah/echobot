b7219bd45088c99de1419b80104de4611690e146
Oleg Pascal <rtrn.0@ya.ru>
2020-09-24 12:08:09 +0300

    Temporarly change project structure to work with multiple Main modules

---
+++ examples/VKMain.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import qualified Data.Configurator as Conf
+import qualified Data.Text.IO      as TIO
+import           VK.Bot
+import           VK.Env
+
+main :: IO ()
+main = do
+  putStrLn "not yet implemented"
