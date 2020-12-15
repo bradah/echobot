b790f41ad4f03c0563b02f36a8c326b37f2dbe01
Oleg Pascal <rtrn.0@ya.ru>
2020-12-15 06:02:49 +0300

    Add BotM monad

--- examples/TelegramMain.hs
+++ examples/TelegramMain.hs
@@ -1,9 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import qualified Data.Configurator as Conf
+import qualified Data.Configurator        as Conf
 import           Telegram.Bot
 import           Telegram.Env
+
+
+import           Network.HTTP.Client      (newManager)
+import           Network.HTTP.Client.TLS  (tlsManagerSettings)
+import           Servant.Client
+import           Telegram.Bot
+import           Telegram.Methods.Request
+import           Telegram.ServMethods
 
 main :: IO ()
 main = do
@@ -12,3 +20,15 @@ main = do
   conf <- Conf.load [Conf.Required "echobot.conf.local"]
   mbToken <- Conf.lookup conf "telegram.token"
   maybe (return ()) (runBot . mkEnv) mbToken
+
+f :: IO ()
+f = do
+  conf <- Conf.load [Conf.Required "echobot.conf.local"]
+  token <- Conf.require conf "telegram.token"
+  let base = botBaseUrl token
+  clientEnv <- mkClientEnv <$> newManager tlsManagerSettings <*> pure base
+  res <- runClientM (getMe >> getUpdates (GetUpdatesBody Nothing Nothing Nothing)) clientEnv
+  case res of
+    Right x -> print x
+    _       -> undefined
+  return ()
--- src/Telegram/Bot.hs
+++ src/Telegram/Bot.hs
@@ -1,7 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+
+
 module Telegram.Bot where
 
+import           Control.Monad.IO.Class
 import           Control.Monad.Reader
+import           Data.Configurator        as Conf
+import           Data.Text                (Text, unpack)
+import           Servant.Client
+
 import           Telegram.Bot.Action
 import           Telegram.Env
 import           Telegram.Methods
@@ -28,3 +37,34 @@ runBot = startBotPolling Nothing
       let offset | null ups = Nothing
                  | otherwise = (1+) <$> updateId <?> last ups
       startBotPolling offset env
+
+
+newtype BotM a = BotM
+  { unBotM :: ReaderT BotSettings ClientM a
+  } deriving (Functor, Applicative, Monad, MonadReader BotSettings, MonadIO)
+
+
+
+data BotSettings = BotSettings
+  { botHandleUpdate :: Update -> Maybe Action
+  , botHandleAction :: Action -> BotM ()
+  , botToken        :: Text
+  }
+
+mkBotSettings
+  :: (Update -> Maybe Action)
+  -> (Action -> BotM ())
+  -> IO BotSettings
+mkBotSettings upHandler actHandler = do
+  conf <- Conf.load [Conf.Required "echobot.conf.local"]
+  BotSettings upHandler actHandler
+    <$> Conf.require conf "telegram.token"
+
+
+{- runBot :: BotSettings -> IO ()
+runBot sets = do
+  let token = botToken sets -}
+
+botBaseUrl :: Token -> BaseUrl
+botBaseUrl token = BaseUrl Https "api.telegram.org" 443
+  (unpack ("/bot" <> token))
